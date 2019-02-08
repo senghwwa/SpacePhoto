34c46a1b96b10298f4cb748a9e42b884d662bd99
senghwwa <senghwabeh@hotmail.com>
2019-02-08 14:22:43 +0800

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Seng Hwwa on 08/02/2019.
+//  Copyright © 2019 senghwabeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
